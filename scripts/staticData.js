4e07e74739a5b7a24716f1285c4629045ab6985f
haben mehar <habenmehari71@gmail.com>
2023-06-01 11:10:09 +0300

    daynamic page images updated and about pages also updated

--- scripts/staticData.js
+++ scripts/staticData.js
@@ -2,37 +2,37 @@
 const testimonials = [
   {
     name: 'Jemal Ahmed',
-    image: '../assets/images/testimonial1.png',
+    image: './assets/images/testimonial1.png',
     task: 'Desk Moving',
     comment: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
   },
   {
     name: 'Winta Lema',
-    image: '../assets/images/testimonial2.png',
+    image: './assets/images/testimonial2.png',
     task: 'Desk Moving',
     comment: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
   },
   {
     name: 'Abrham Beyene',
-    image: '../assets/images/testimonial2.png',
+    image: './assets/images/testimonial2.png',
     task: 'Desk Moving',
     comment: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
   },
   {
     name: 'Hayelom Zenebe',
-    image: '../assets/images/testimonial1.png',
+    image: './assets/images/testimonial1.png',
     task: 'Desk Moving',
     comment: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
   },
   {
     name: 'Elias Kindeya',
-    image: '../assets/images/testimonial1.png',
+    image: './assets/images/testimonial1.png',
     task: 'Desk Moving',
     comment: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
   },
   {
     name: 'Marta Mehari',
-    image: '../assets/images/testimonial2.png',
+    image: './assets/images/testimonial2.png',
     task: 'Desk Moving',
     comment: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
   },
